3784a6b482dc0211c4a253dbc89e8bc2aac08340
Mohammadarifcoding <minaraarifa@gmail.com>
2023-12-09 00:04:34 +0600

    Added the search functionality for the donor page

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,20 @@
+const { getDonorData } = require("../lib/users");
+const DonursCollection = require("../models/Donors");
+
 const exampleDataApi = async(req, res) =>{
     // you  can call the function form the lib for logic here
     console.log('reques accespt ',req.params.id);
     res.send({message : 'hello word server is running...'})
 } 
 
+const donourInfo = async(req,res)=>{
 
-module.exports = {exampleDataApi}
+    const queryValue = req.query
+   const result = await getDonorData(queryValue)
+    
+    console.log(result)
+   res.send(result)
+
+}
+
+module.exports = {exampleDataApi,donourInfo}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
-const { exampleDataApi } = require('../api')
+const { exampleDataApi, donourInfo } = require('../api')
+
 
 const router = require('express').Router()
 
 router.get('/data/:id',exampleDataApi)
 
+
+router.get('/donurs',donourInfo)
 
 
 module.exports = router
